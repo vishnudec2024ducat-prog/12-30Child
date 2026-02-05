5ffa2547d39183bbe2aa8043face3336596726cd
Vishnu <vishnudec2024ducat@gmail.com>
2026-02-05 13:24:15 +0530

    modal form nbame  changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import { Outlet } from 'react-router-dom'
-import ModalForm from './Components/modalForm'
+import ModalForm from './Components/ModalForm'
 
 const App = () => {
   return (
